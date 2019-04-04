153079a9e7eecea70861ac1498e3decd49481ddb
1512289-NguyenThiMyLinh <keochua45@gmail.com>
2019-04-04 13:26:41 +0700

    Update Calendar

--- UMC.STTapplication/feature/src/main/java/stt/umc/feature/Booking.java
+++ UMC.STTapplication/feature/src/main/java/stt/umc/feature/Booking.java
@@ -1,11 +1,18 @@
 package stt.umc.feature;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.DialogFragment;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import java.util.Calendar;
+import java.text.DateFormat;
 
-public class Booking extends AppCompatActivity {
+import stt.umc.feature.fragments.DatePickerFragmen;
+
+public class Booking extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
 
     private Button buttonBooking;
     private View bookingCalendarView;
@@ -18,8 +25,19 @@ public class Booking extends AppCompatActivity {
         buttonBooking.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bookingCalendarView.setVisibility(View.VISIBLE);
+                //bookingCalendarView.setVisibility(View.VISIBLE);
+                DialogFragment datePicker = new DatePickerFragmen();
+                datePicker.show(getSupportFragmentManager(), "date picker");
             }
         });
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        String currentDateString = DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
     }
 }
